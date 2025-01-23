fa452b5ea9aeca09795a37e6a2a43f14f44fd332
Kobe Anderson <kobeanderson51@gmail.com>
2025-01-23 09:17:38 -0700

    fix deploy api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  production: false,
-  // Personal note: Keep API keys secure and out of version control
+  production: true,
   newsApiKey: process.env['NG_APP_NEWS_API_KEY'] || '',
   firebase: JSON.parse(process.env['NG_APP_FIREBASE_CONFIG'] || '{}')
 };
